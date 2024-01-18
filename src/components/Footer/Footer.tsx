d7d63d506d9ca31da5cbb3865c3e79f91f24cf53
Cezar Ferreira <cezarleferr@gmail.com>
2024-01-18 11:11:44 -0400

    footer finalizado com responsividade os devidos links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,65 +1,74 @@
 import Image from 'next/image'
 import styles from './Footer.module.css'
 import Link from 'next/link'
+import { linkFacebook, linkInstagram, linkTelegram, linkWpp } from '@/utils/links'
 
 export default function Footer() {
   return <>
     <footer className={styles.footer}>
-      <Image
-        src="/images/nanoservice-logo.png"
-        alt="nanoservice logo"
-        width={350}
-        height={350}
-        quality={100}
-      />
+      <div className={styles.content}>
+        <Image
+          src="/images/nanoservice-logo.png"
+          alt="nanoservice logo"
+          width={350}
+          height={350}
+          quality={100}
+          className={styles.logo}
+        />
 
-      {/*Adicionar links aos icones das redes sociais*/}
-      <section className={styles.contactSection}>
-        <h2>Contato</h2>
-        <div className={styles.contactMeans}>
-          WhatsApp e Telegram: (92) 92985-3348
-          <br /> <br />
-          E-mail: comercial@nanoservice.net.br
-        </div>
-        <div className={styles.socialMedias}>
-          <Link href="#">
-            <Image
-              src="/images/icon-instagram.png"
-              alt="icone instagram"
-              quality={100}
-              width={90}
-              height={90}
-            />
-          </Link>
-          <Link href="#">
-            <Image
-              src="/images/icon-facebook.png"
-              alt="icone facebook"
-              quality={100}
-              width={90}
-              height={90}
-            />
-          </Link>
-          <Link href="#">
-            <Image
-              src="/images/icon-whatsapp.png"
-              alt="icone whatsapp"
-              quality={100}
-              width={90}
-              height={90}
-            />
-          </Link>
-          <Link href="#">
-            <Image
-              src="/images/icon-telegram.png"
-              alt="icone telegram"
-              quality={100}
-              width={90}
-              height={90}
-            />
-          </Link>
-        </div>
-      </section>
+        <section className={styles.contactSection}>
+          <h2>Contato</h2>
+          <div className={styles.contactMeans}>
+            WhatsApp e Telegram: (92) 92985-3348
+            <br /> <br />
+            E-mail: comercial@nanoservice.net.br
+          </div>
+          <div className={styles.socialMedias}>
+            <Link href={linkInstagram} target='_blank'>
+              <Image
+                src="/images/icon-instagram.png"
+                alt="icone instagram"
+                quality={100}
+                width={90}
+                height={90}
+              />
+            </Link>
+            <Link href={linkFacebook} target='_blank'>
+              <Image
+                src="/images/icon-facebook.png"
+                alt="icone facebook"
+                quality={100}
+                width={90}
+                height={90}
+              />
+            </Link>
+            <Link href={linkWpp} target='_blank'>
+              <Image
+                src="/images/icon-whatsapp.png"
+                alt="icone whatsapp"
+                quality={100}
+                width={90}
+                height={90}
+              />
+            </Link>
+            <Link href={linkTelegram} target='_blank'>
+              <Image
+                src="/images/icon-telegram.png"
+                alt="icone telegram"
+                quality={100}
+                width={90}
+                height={90}
+              />
+            </Link>
+          </div>
+        </section>
+
+      </div>
+      <div className={styles.copyright}>
+        © Copyright NanoTech. Todos os direitos reservados
+        <br />
+        Desenvolvido por NanoTech
+      </div>
     </footer>
   </>
 }
