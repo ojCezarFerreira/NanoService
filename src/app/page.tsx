7615927e1c7fb6002ed5a94dbd4d950f793ecec0
Cezar Ferreira <cezarleferr@gmail.com>
2024-01-18 23:27:46 -0400

    banner adicionado e bugs do menu mobile e desktop resolvidos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
 import styles from './page.module.css'
+
+import banner from '../../public/images/banner-principal-assistencia-tecnica-manaus.png'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      NANO
+      <section className={styles.banner}>
+        <Image
+          src={banner}
+          alt="banner principal assistencia tecnica manaus"
+          fill={true}
+          quality={100}
+          placeholder='blur'
+        />
+      </section>
     </main>
   )
 }
